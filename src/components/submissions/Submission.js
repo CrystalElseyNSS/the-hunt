1fbf9af119da70d2ab8a10baaeee7cc62c978556
Crystal Elsey <hello@crystalelsey.com>
2020-08-13 15:51:37 -0500

    converting submissions to table format

--- src/components/submissions/Submission.js
+++ src/components/submissions/Submission.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react"
 import { format } from "date-fns"
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap"
+import { Button, Modal, ModalHeader, ModalBody, ListGroup, ListGroupItem } from "reactstrap"
 import { EditSubmissionForm } from "../submissions/EditSubmissionForm"
 import { CompanyContext } from "../companies/CompanyProvider"
 import { SubmissionContext } from "./SubmissionProvider"
@@ -8,7 +8,7 @@ import SubmissionTaskList from "../submissionTasks/SubmissionTaskList"
 import "./Submission.css"
 
 export default (props) => {
-    
+
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
     const [editModal, setEditModal] = useState(false)
@@ -18,52 +18,61 @@ export default (props) => {
     const { submissions, deleteSubmission } = useContext(SubmissionContext)
     const foundCompany = companies.find(co => co.id === props.submission.companyId) || {}
     const formattedAppDate = format(new Date(props.submission.dateApplied), "MM/dd/yyyy")
-    
+
     useEffect(() => {
         const savedSubmission = submissions.find(saved => saved.id === selectedSubmission.id)
         setSubmission(savedSubmission)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [submissions])
-    
+
     useEffect(() => {
         const deletedSubmission = submissions.find(deleted => deleted.id === selectedSubmission.id)
         setSubmission(deletedSubmission)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [submissions])
-    
+
     return (
         <>
-            <section key={props.submission.id} className="submission">
-                <div className="submission__company">Company: {foundCompany.companyName}</div>
-                <div className="submission__position">Position: {props.submission.position}</div>
-                <div className="submission__position">Source: {props.submission.source}</div>
-                <div className="submission__position">Referral: {props.submission.referral}</div>
-                <div className="submission__dateApplied">Date Applied: {formattedAppDate}</div>
-                <Button onClick={toggle} color="danger" size="sm">To Do</Button>{' '}
-                <Button 
-                    color="info" 
-                    size="sm"
-                    onClick={() => {
-                        toggleEdit()}}
+
+            <ListGroup key={props.submission.id} horizontal className="submission">
+                <div>{foundCompany.companyName} : </div>
+                <div>{props.submission.position}</div>
+                <div>{formattedAppDate}</div>
+                <ListGroup horizontal>
+                    <div className="icon--submission">
+                        <i className="fas fa-exclamation-circle" aria-hidden="true" onClick={toggle}></i>
+                    </div>
+                    {/* <Button onClick={toggle} color="danger" size="sm">To Do</Button>{' '} */}
+                    <Button
+                        color="info"
+                        size="sm"
+                        onClick={() => {
+                            toggleEdit()
+                        }}
                     >Edit
-                </Button>{' '}
-                <Button 
-                    color="info" 
-                    size="sm"
-                    onClick={() => {
-                        deleteSubmission(props.submission.id)
-                    }}>Delete
-                </Button>
-            </section>
+                        </Button>{' '}
+                    <Button
+                        color="info"
+                        size="sm"
+                        onClick={() => {
+                            deleteSubmission(props.submission.id)
+                        }}>Delete
+                        </Button>
+                    </ListGroup>
+            </ListGroup>
+            {/* <div className="submission__position">Source: {props.submission.source}</div>
+                <div className="submission__position">Referral: {props.submission.referral}</div> */}
+            {/* <div className="submission__dateApplied">Date Applied: {formattedAppDate}</div> */}
+
 
             <Modal isOpen={modal} toggle={toggle}>
                 <ModalHeader toggle={toggle}>
                     <p className="form--heading">Target Practice:</p>
                 </ModalHeader>
                 <ModalBody>
-                    <SubmissionTaskList submissionId={props.submission.id} toggler={toggle} />                    
+                    <SubmissionTaskList submissionId={props.submission.id} toggler={toggle} />
                 </ModalBody>
-            </Modal>  
+            </Modal>
 
             <Modal isOpen={editModal} toggle={toggleEdit}>
                 <ModalBody>
@@ -71,7 +80,7 @@ export default (props) => {
                 </ModalBody>
             </Modal>
         </>
-        
+
     )
-    
+
 }   
